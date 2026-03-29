19fcc292dda698e9f832254747dc7dc6b88ccc80
vietnguyen <vietnguyendl61@gmail.com>
2026-03-29 13:54:35 +0700

    setup otel collector

--- internal/cron/jobs/cleanup.go
+++ internal/cron/jobs/cleanup.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"log"
 	"time"
-	"trace-log/internal/telemetry"
 )
 
 func CleanupTask(ctx context.Context) {
 	// You can now create child spans easily
-	_, dbSpan := telemetry.Tracer.Start(ctx, "database.cleanup_old_records")
-	defer dbSpan.End()
+	//_, dbSpan := telemetry.Tracer.Start(ctx, "database.cleanup_old_records")
+	//defer dbSpan.End()
 
 	// Simulate work
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 	log.Println("Cleanup completed")
 }
